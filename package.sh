71f553e64e46f3fe26bb3bfba006004b78d13b54
Antonio Galea <antonio.galea@gmail.com>
2014-12-02 19:27:35 +0100

    check for uncommited changes before packaging

--- package.sh
+++ package.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ "`git status --porcelain|wc -l`" != "0" ]; then
+ echo "Your git repository has uncommited changes - aborting."
+ exit 1
+fi
 VERSION=`git describe --always`
 NAME=websup-git-$VERSION.tgz
 echo "Git revision: $VERSION" > VERSION.txt

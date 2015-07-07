5b202f60ee40e1402b480dd37c59fbb90f21c71d
Antonio Galea <antonio.galea@gmail.com>
2015-07-07 10:15:55 +0200

    serialization class

--- cli/db.py
+++ cli/db.py
@@ -7,24 +7,38 @@ class Database:
         self.conn = sqlite3.connect(self.dbfile)
         self.conn.row_factory = sqlite3.Row
         cursor = self.conn.cursor()
-        cursor.execute("""
-CREATE TABLE IF NOT EXISTS messages (timestamp TEXT, message TEXT);
-CREATE INDEX IF NOT EXISTS messages_timestamp_idx ON messages (timestamp);
-        """)
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS messages "
+            "(timestamp TEXT, message TEXT);"
+        )
+        cursor.execute(
+            "CREATE INDEX IF NOT EXISTS messages_timestamp_idx "
+            "ON messages (timestamp);"
+        )
         self.conn.commit()
 
     def __del__(self):
         if self.conn:
             self.conn.close()   
+            self.conn = None
 
-    def messages(date='now'):
+    def messages(self, date='now'):
         cursor = self.conn.cursor()
         rows = cursor.execute(
-            "SELECT * FROM messages WHERE date(timestamp) = date(?)"
-            "  ORDER BY timestamp DESC",
+            "SELECT * FROM messages "
+            "WHERE date(timestamp) = date(?) "
+            "ORDER BY timestamp DESC",
             [date]
-        )
-        row = True
-        while row:
-            row = rows.fetchone()
-            yield row
+        ).fetchall()
+        return [ dict(row) for row in rows ]
+
+    def save(self, item):
+        if item.item_type == 'message':
+            timestamp = item.content['timestamp']
+            message   = item.asJson()
+            cursor = self.conn.cursor()
+            cursor.execute(
+                "INSERT INTO messages VALUES (?,?)",
+                [timestamp, message]
+            )
+            self.conn.commit()
